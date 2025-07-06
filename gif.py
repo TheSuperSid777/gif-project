484cb39b4f722995d914125d757be29d096701af
Siddarth_tak <siddarth.tak03@gmail.com>
2025-07-06 14:04:03 +0530

    hi

--- gif.py
+++ gif.py
@@ -1,6 +1,6 @@
 import imageio.v3 as iio
-images = ["image1.jpeg","image2.jpeg"]
+images = ['image1.png','image2.png']
 gifs = []
 for i in images:
-    gifs.append(iio.imread(images))
-iio.imwrite("my_gif",gifs,duration = 500,loop = 0)
+    gifs.append(iio.imread(i))
+iio.imwrite("my_gif.gif",gifs,duration = 500,loop = 0)
